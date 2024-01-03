48d5dfbefa1b4963d5becd74b98137dc9458765d
ThanaaAdel <swaadl971@gmail.com>
2024-01-03 11:15:19 +0200

    rotaion transmaition

--- lib/animation/slide_right.dart
+++ lib/animation/slide_right.dart
@@ -7,10 +7,10 @@ class SlideRight extends PageRouteBuilder {
     pageBuilder: (context, animation, animationtwo) => page,
     transitionsBuilder: (context, animation, animationtwo, child) {
       var begin = 0.0;
-      var end = 5.0;
+      var end = 1.0;
       var tween = Tween(begin: begin, end: end);
       var curveAnimations = CurvedAnimation(parent: animation, curve: Curves.easeInCirc);
-      return ScaleTransition(scale: tween.animate(curveAnimations), child: child);
+      return RotationTransition(turns: tween.animate(curveAnimations), child: child);
     },
   );
 }
