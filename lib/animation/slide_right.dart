bffafa61564cc87c30553a152fb9f4c4d25f3a34
ThanaaAdel <swaadl971@gmail.com>
2024-01-03 11:08:58 +0200

    slide transmation

--- lib/animation/slide_right.dart
+++ lib/animation/slide_right.dart
@@ -6,11 +6,11 @@ class SlideRight extends PageRouteBuilder {
       : super(
     pageBuilder: (context, animation, animationtwo) => page,
     transitionsBuilder: (context, animation, animationtwo, child) {
-      var begin = const Offset(-1.0, 1.0);
+      var begin = const Offset(0.0, 1.0);
       var end = const Offset(0.0, 0.0);
       var tween = Tween(begin: begin, end: end);
-      var offsetAnimation = animation.drive(tween);
-      return SlideTransition(position: offsetAnimation, child: child);
+      var curveAnimations = CurvedAnimation(parent: animation, curve: Curves.easeInCirc);
+      return SlideTransition(position: tween.animate(curveAnimations), child: child);
     },
   );
 }
